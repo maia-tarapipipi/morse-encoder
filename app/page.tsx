65c15011bbea17bbd7c50d6fc17901857b7cffdb
maia-tarapipipi <tarapipipim@gmail.com>
2023-08-02 12:09:45 +1200

    project boilerplate initialization

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useState } from 'react'
+
+function Page() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div>
+    <h1>Hello, Next.js!</h1>
+  </div>)
+}
+
+export default Page
